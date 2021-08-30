6ef2c1a1e67655bbef38614cea7ac6463fca4187
parag <parag@DESKTOP-I2Q9QKQ>
2021-08-30 16:49:46 +0530

    Add project files.

---
+++ bitqueueex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace bitqueueex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Queue my_queue = new Queue();
+            my_queue.Enqueue("samu");
+            my_queue.Enqueue(2);
+            my_queue.Enqueue(200);
+            my_queue.Enqueue(null);
+            my_queue.Enqueue(4.4);
+            my_queue.Enqueue("SAMIKSHA");
+            foreach (var ele in my_queue)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
